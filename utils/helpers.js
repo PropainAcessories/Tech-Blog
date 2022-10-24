9d278578265c7d5691b549c8f456001e8269d80c
HankHill <hhc3h8@gmail.com>
2022-10-24 10:44:19 -0500

    can only datestamp current date like idiot

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,12 +2,15 @@ module.exports = {
     // Helper functions
     format_date: () => {
         const dayjs = require('dayjs');
-        let now = dayjs().format('MMMM-DD-YYYY hh:mm').toString();
+        let now = dayjs().format('MMMM-DD-YYYY hh:mm A').toString();
 
         const date = new Date(now);
         console.log(date);
 
-        return date;
+        const dateStamp = dayjs(date);
+        console.log(dateStamp);
+
+        return dayjs(dateStamp).format('MMMM-DD-YYYY hh:mm A');
     },
     format_multiple: (word, amount) => {
         if (amount !== 1) {
