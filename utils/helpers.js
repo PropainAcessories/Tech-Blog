024c7b258a81f8f96ee9f542d09b7c42a28dfc4f
HankHill <hhc3h8@gmail.com>
2022-10-24 08:59:23 -0500

    datestamping

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
 module.exports = {
     // Helper functions
+    format_date: () => {
+        const dayjs = require('dayjs');
+        let now = dayjs().format('MMMM-DD-YYYY hh:mm A').toString();
+
+        console.log(now);
+
+        return now;
+    },
     format_multiple: (word, amount) => {
         if (amount !== 1) {
             return `${word}s`;
